729b64d8830d5e596be370a036dd1e9bc78f7d22
Amith501 <user.b.amitraj25@gmail.com>
2025-09-16 21:48:34 +0530

    updated

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ function scrollToOptions(id) {
   if (section) {
     section.scrollIntoView({ behavior: "smooth" });
   }
+
 }
+
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const contactSection = document.querySelector(".contact-section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            contactSection.classList.add("show");
+          }
+        });
+      },
+      { threshold: 0.2 } // triggers when 20% of section is visible
+    );
+
+    if (contactSection) {
+      observer.observe(contactSection);
+    }
+  });
+
