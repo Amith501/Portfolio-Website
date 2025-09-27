852237b7eadb050105c61c9b97a02497043a7621
Amith501 <user.b.amitraj25@gmail.com>
2025-09-27 22:09:30 +0530

    updated

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ function scrollToOptions(id) {
   if (section) {
     section.scrollIntoView({ behavior: "smooth" });
   }
-
 }
 
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const contactSection = document.querySelector(".contact-section");
+document.addEventListener("DOMContentLoaded", () => {
+  const contactSection = document.querySelector(".contact-section");
 
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            contactSection.classList.add("show");
-          }
-        });
-      },
-      { threshold: 0.2 } // triggers when 20% of section is visible
-    );
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          contactSection.classList.add("show");
+        }
+      });
+    },
+    { threshold: 0.2 } // triggers when 20% of section is visible
+  );
 
-    if (contactSection) {
-      observer.observe(contactSection);
-    }
-  });
-
+  if (contactSection) {
+    observer.observe(contactSection);
+  }
+});
